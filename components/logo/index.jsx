aa2aa16c40439ec490a4c2939fe0ff9a9c687b09
Adam Recvlohe <adam.recvlohe@pm.me>
2022-12-03 13:04:34 -0600

    Add aria-label for logo

--- components/logo/index.jsx
+++ components/logo/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Logo = (_props) => {
-  return <img width={250} src="/opengraph.png" />;
+  return (
+    <img
+      width={250}
+      src="/opengraph.png"
+      aria-label="Native Talent in Tech Logo"
+    />
+  );
 };
 
 export default Logo;
